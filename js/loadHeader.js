3997051470e76a64c9e96599f852c6989c8db43e
TheYarnMagpie <stitchinspire.admn@gmail.com>
2025-05-10 00:31:19 +0200

    fix js

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const path = window.location.pathname.includes('/pages/') ? '../components/header.html' : 'components/header.html';
 
     fetch(path)
-                .then(response => {
-                      if (!response.ok) {
-                              throw new Error(`HTTP error! Status: ${response.status}`);
-                                    }
-                                          return response.text();
-                                              })
-                                                  .then(data => {
-                                                        headerPlaceholder.innerHTML = data;
-                                                            })
-                                                                .catch(error => {
-                                                                      console.error('Error loading header:', error);
-                                                                          });
-                                                                          });
-})
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        headerPlaceholder.innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading header:', error);
+      });
+    });
+    
